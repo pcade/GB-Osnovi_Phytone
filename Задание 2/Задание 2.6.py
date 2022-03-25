5b283986714c6c6da0c256b994752016d4e54118
Pcade <grishamindme@gmail.com>
2022-03-25 13:28:07 +0300

    доделал шестое задание

--- Задание 2/Задание 2.6.py
+++ Задание 2/Задание 2.6.py
@@ -1,7 +1,8 @@
+from itertools import chain
+from collections import defaultdict
 
 mylist = []
 Flag = True
-counter = 1
 
 while Flag:
     name = input("Введите наименование - ")
@@ -10,38 +11,22 @@ while Flag:
     kinds = input("Введите систему измерения - ")
     if input("Желаете заполнить ещё ? Д/Н ") == "Н":
         Flag = False
-    mydict = {"наименование": name, "цена": price, "колличество": numbers, "ед": kinds}
-    mycorteg = (counter, mydict)
+    mydict = {"наименование": name, "цена": price, "количество": numbers, "ед": kinds}
+    mycorteg = (mydict)
     mylist.extend([mycorteg])
-    counter += 1
-    print(mylist)
-dict1 = mylist[0][1]
-dict2 = (mylist[1][1])
-dict1.update(dict2)
-print(dict1)
-print(dict2)
 
+superdict = {}
+list_name, list_prise, list_amount, list_unit = [], [], [], []
+for i in range(len(mylist)):
+    list_name.append(mylist[i].get('наименование'))
+    list_prise.append(mylist[i].get('цена'))
+    list_amount.append(mylist[i].get('количество'))
+    list_unit.append(mylist[i].get('ед'))
 
+superdict['наименование'] = list_name
+superdict['цена'] = list_prise
+superdict['количество'] = list_amount
+superdict['ед'] = list_unit
 
-
-
-
-""""
-i = len(mylist)
-print(numbers_of_elements)
-dict1 = (mylist[0][1])
-dict2 = (mylist[1][1])
-dict1.update(dict2)
-print(dict1)
-"""
-
-
-"""
-if input("Вы хотите заполнить стуктуру? Да/Нет - ") == 'Да':
-    name = input("Введите наименование - ")
-    price = input("Введите стоимость - ")
-    numbers = input("Введите колличество - ")
-    kinds = input("Введите систему измерения - ")
-mylist = [("наименование", name), ("цена" , price), ("колличество" , numbers), ("ед" ,kinds)]
-print(mylist)
-"""
+for k, v in superdict.items():
+    print(k, v)
