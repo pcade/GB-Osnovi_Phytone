805f79aee580bbda7290f8dcd01f79ddc46dd16d
Pcade <grishamindme@gmail.com>
2022-04-25 23:33:36 +0300

    решены задачи 1 и 2

---
+++ Задание 4/Задание 4.1.py
@@ -0,0 +1,17 @@
+flag = True
+
+last_sallary = input("сколько Вы зароботали за прошлый месяц - ")
+present_sallary = input("из них премия составила - ")
+type_work = input("Введите по какому граффику Вы работаете(2/2, 5/2, 1/3) - ")
+period = input("За какой пероид произвести рассчёт в дня - ")
+answer = 0
+
+if type_work == "2/2":
+    answer = int(last_sallary)  // 15 * int(period)
+    print(f"За {period} смен, Вы заработаете - {answer} руб.")
+elif type_work == "5/2":
+    answer = int(last_sallary) // 22 * int(period)
+    print(f"За {period} смен, Вы заработаете - {answer} руб.")
+else:
+    answer = int(last_sallary) // 8 * int(period)
+    print(f"За {period} смен, Вы заработаете - {answer} руб.")
